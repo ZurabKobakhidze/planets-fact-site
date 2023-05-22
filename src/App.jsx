5474dbf3a58ebcc9b77f1c345ba91cf0354a4fda
Zukak <zukakobakhidze@gmail.com>
2023-05-22 19:20:12 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import { Routes, Route} from "react-router-dom";
+
+function App() {
+  
+  return <div>
+    <Routes>
+      <Route path="/" element={<div>main page</div>}/>
+    </Routes>
+
+  </div>;
+}
+
+export default App;
