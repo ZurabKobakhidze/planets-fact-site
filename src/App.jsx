4692a85c56a18be2e0797ce4a28f5a8025142647
Zukak <zukakobakhidze@gmail.com>
2023-05-23 17:52:20 +0400

    feat: add functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
-import Planets from "./planets"; // Update import statement
-import { Routes, Route, Navigate } from "react-router-dom";
+import PlanetDetails from "./PlanetDetails";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Hamburger from "./assets/icon-hamburger.svg";
 
 function App() {
+  const navigate = useNavigate();
+
+  const handlePlanetClick = (planetName) => {
+    navigate(`/PlanetDetails/${planetName}`);
+  };
+
   return (
     <div>
       <header className="header">
@@ -11,10 +17,36 @@ function App() {
         <div className="menu_div">
           <img src={Hamburger} alt="Hamburger" />
         </div>
+        <div className="menu_planets">
+          <button className="menu_names" onClick={() => handlePlanetClick("mercury")}>
+            mercury
+          </button>
+          <button className="menu_names" onClick={() => handlePlanetClick("venus")}>
+            venus
+          </button>
+          <button className="menu_names" onClick={() => handlePlanetClick("earth")}>
+            earth
+          </button>
+          <button className="menu_names" onClick={() => handlePlanetClick("mars")}>
+            mars
+          </button>
+          <button className="menu_names" onClick={() => handlePlanetClick("jupiter")}>
+            jupiter
+          </button>
+          <button className="menu_names" onClick={() => handlePlanetClick("saturn")}>
+            saturn
+          </button>
+          <button className="menu_names" onClick={() => handlePlanetClick("uranus")}>
+            uranus
+          </button>
+          <button className="menu_names" onClick={() => handlePlanetClick("neptune")}>
+            neptune
+          </button>
+        </div>
       </header>
       <Routes>
-        <Route path="/" element={<Navigate to="/planets" />} />
-        <Route path="/planets" element={<Planets />} /> {/* Update component name */}
+        <Route path="/" element={<PlanetDetails />} />
+        <Route path="/PlanetDetails/:planetName" element={<PlanetDetails />} />
       </Routes>
     </div>
   );
