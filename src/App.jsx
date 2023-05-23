dcd9e6677123bd40487f679ad5acc8cb6642b6bf
Zukak <zukakobakhidze@gmail.com>
2023-05-23 13:55:46 +0400

    feat: add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import "./App.css";
 import planets from "./planets";
-import { Routes, Route, Navigate} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Hamburger from "./assets/icon-hamburger.svg";
 
 function App() {
-  
-  return <div>
-    <Routes>
-      <Route path="/" element={<Navigate to="/planets"/>}/>
-      <Route path="/planets" element={<planets/>}/>
-    </Routes>
+  return (
+    <div>
+      <header className="header">
+        <h1 className="h1_planets">THE PLANETS</h1>
+        <div className="menu_div">
+          <img src={Hamburger} alt="Hamburger" />
+        </div>
+      </header>
+      <div className="underline"></div>
+      <div className="info_planets_div">
+        <h2 className="h2_info">OVERVIEW</h2>
+        <h2 className="h2_info">Structure</h2>
+        <h2 className="h2_info">Surface </h2>
+      </div>
+      <div className="underline"></div>
 
-  </div>;
+      <Routes>
+        <Route path="/" element={<Navigate to="/planets" />} />
+        <Route path="/planets" element={<planets />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
